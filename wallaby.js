3ad2d44f5cee2ba46a66905f41707edd04ce6598
Khoa Huynh <khoawin92@gmail.com>
2018-06-26 23:10:19 -0700

    file structure set up

---
+++ wallaby.js
@@ -0,0 +1,36 @@
+module.exports = function (wallaby) {
+  return {
+    files: [
+      'src/**/*.js',
+      '!__test__/**/*.spec.js',
+      'index.js',
+      'public/*.html',
+      'data/*.json',
+    ],
+
+    tests: [
+      '__test__/**/*.spec.js',
+    ],
+
+    testFramework: 'jest',
+    env: {
+      type: 'node',
+    },
+
+    compilers: {
+      '**/*.js': wallaby.compilers.babel({
+        'presets': [
+          'env',
+          'react',
+          'stage-0',
+        ],
+        'plugins': [
+          'transform-class-properties',
+          'transform-decorators',
+          'transform-react-constant-elements',
+          'transform-react-inline-elements',
+        ],
+      }),
+    },
+  };
+};
